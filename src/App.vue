0dfe331b5935b825fa8f608cb23f6c42de0e0b04
orch20 <94862153+orch20@users.noreply.github.com>
2023-09-12 22:46:28 +0300

    added initial files, pages Note and stats, component NavBar, also implement routes and styled components

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <NavBar />
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import NavBar from './components/Layout/NavBar.vue';
+</script>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
---
+++ src/components/Layout/NavBar.vue
@@ -0,0 +1,63 @@
+<template>
+    <nav
+      class="navbar is-success"
+      aria-label="main navigation"
+      role="navigation"
+    >
+        <div class="container is-max-desktop px-2">
+            <div class="navbar-brand">
+            <div class="navbar-item is-size-4 is-family-monospace" >
+              Noteball
+            </div>
+
+            <a
+              @click.prevent="showMobileNav = !showMobileNav"
+              :class="{'is-active': showMobileNav}"
+              class="navbar-burger"
+              aria-expanded="false"
+              aria-label="menu"
+              data-target="navbarBasicExample"
+              role="button"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showMobileNav }">
+       
+
+            <div class="navbar-end">
+                <RouterLink to="/" class="navbar-item" active-class="is-active">
+                    Notes
+                    </RouterLink>
+                <RouterLink to="/stats" class="navbar-item" active-class="is-active">
+                    Stats
+                </RouterLink>
+             </div>
+          </div>
+        </div>
+      
+    </nav>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const showMobileNav = ref(false);
+
+
+</script>
+
+<style>
+@media (max-width: 1023px) {
+    .navbar-menu {
+        position: absolute;
+        left: 0;
+        width: 100%;
+    }
+
+}
+
+</style>
