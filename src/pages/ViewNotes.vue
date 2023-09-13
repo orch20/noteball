2ec332c531add1d944f2a80164227f41064b846c
orch20 <94862153+orch20@users.noreply.github.com>
2023-09-13 11:41:43 +0300

    added feature of adding new note

--- src/pages/ViewNotes.vue
+++ src/pages/ViewNotes.vue
@@ -3,22 +3,32 @@
         <div class="card has-background-success-dark p-4 mb-5">
             <div class="field">
                 <div class="control">
-                    <textarea class="textarea" placeholder="Add a new note"/>
+                   <textarea
+                      v-model="newNote"  
+                      class="textarea"
+                      placeholder="Add a new note"
+                      ref="newNoteInputFocus"
+                    />
                 </div>
                 </div>
 
                 <div class="field is-grouped is-grouped-right">
                 <div class="control">
-                    <button class="button is-link has-background-success">Add New Note</button>
+                    <button
+                      @click.prevent="addNewNote"
+                      :disabled="!newNote"
+                      class="button is-link has-background-success">Add New Note
+                      </button
+                    >
                 </div>
                 
             </div>
         </div>
 
-        <div class="card mb-4">
+        <div class="card mb-4" v-for="note in notes" :key="note.id">
             <div class="card-content">
             <div class="content">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam repellendus temporibus dolorem reprehenderit obcaecati quis nemo, itaque neque provident iusto adipisci, minus, odio numquam tenetur suscipit laudantium laborum ullam voluptatibus?
+                {{ note.content }}
             </div>
             </div>
             <footer class="card-footer">
@@ -28,3 +38,34 @@
         </div>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const notes = ref([
+    {
+        id: 'id1',
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Magnam repellendus temporibus dolorem reprehenderit obcaecati quis nemo, itaque neque provident iusto adipisci, minus, odio numquam tenetur suscipit laudantium laborum ullam voluptatibus?",
+    },
+    {
+        id: 'id2',
+        content: "Magnam repellendus temporibus dolorem reprehenderit obcaecati quis nemo, itaque neque provident iusto adipisci, minus, odio numquam tenetur suscipit laudantium laborum ullam voluptatibus?",
+    }
+])
+
+const newNote = ref('')
+const newNoteInputFocus = ref(null)
+
+const addNewNote = () => {
+    const note = {
+        id: Date.now().toString(),
+        content: newNote.value
+    }
+
+    notes.value.unshift(note)
+    newNote.value = ''
+    newNoteInputFocus.value.focus()
+    
+}
+
+</script>
